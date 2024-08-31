26bfb824669c6e965bcbde3cc9b87c60d230632e
Hermann Kao <itishermann@gmail.com>
2024-08-31 10:52:01 +0200

    fix build issue

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,10 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default {
+  serverBuildTarget: 'node-cjs',
+  server: './server.js',
   ignoredRouteFiles: ['.*'],
   devServerPort: 3000,
   tailwind: true,
+  devServerBroadcastDelay: 1000,
 };
